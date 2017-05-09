d6f14525e16db39ec318e8aba7229f77af3fe266
Gaby Vazquez <gabs.vz@gmail.com>
2017-05-08 18:00:32 -0700

    Improving documentation.

--- README.md
+++ README.md
@@ -7,12 +7,19 @@ This project uses gulp to automate some tasks that are useful for front end deve
 * Bundle and minify JavaScript
 * Optimize images for the web
 * Move minified files to a specific directory
-* Automatically reload the browser when files change
+* Automatically reload the browser when files change 
 
 Since you cannot always install gulp globally, this one uses local gulp to run all these tasks.
  
 ## To use it
 
-* Clone this repo 
-* Install its dependencies by running `yarn`/`npm install`
-* Run `yarn gulp` to run the tasks defined in `gulpfile.js`
+* Clone this repo into a folder named as your project and cd into it
+
+```
+git clone https://github.com/gabyvs/gulp-starter my-project-name && cd $_
+```
+
+* Create your own repo from it by running `rm -rf .git` and then run `git init`
+* Install its dependencies by running either `yarn` or `npm install`
+* Run `yarn gulp` or `npm run gulp` to run the tasks defined in `gulpfile.js`
+* Start writing your application code in index.html or src folder
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const minifyCss = require('gulp-minify-css');
-const sourcemaps = require('gulp-sourcemaps');
-const uglify = require('gulp-uglify');
-const imageMin = require('gulp-imagemin');
-const less = require('gulp-less');
+const browserSync   = require('browser-sync').create();
+const gulp          = require('gulp');
+const imageMin      = require('gulp-imagemin');
+const less          = require('gulp-less');
+const minifyCss     = require('gulp-minify-css');
+const sourcemaps    = require('gulp-sourcemaps');
+const uglify        = require('gulp-uglify');
 
-// Minifying images
+/********** IMAGES *********************
+ *
+ * Read original image files from /src/img/ folder
+ * Pass them trough imageMin
+ * Save the minified images into dist/img subfolder
+ * Continue with browserSync logic only after all previous things have finished
+ *
+ ***************************************/
 gulp.task('images', () => {
     gulp.src(['src/img/**/*'])
         .pipe(imageMin())
@@ -14,9 +21,19 @@ gulp.task('images', () => {
         .pipe(browserSync.stream());
 });
 
-// Minifying css and adding sourcemaps
+/********** STYLES *********************
+ *
+ * Read main.less file from /src/styles folder
+ * Create sourcemaps with the original content
+ * Compile Less and generate CSS content instead
+ * Minify the new CSS generated styles
+ * Write the sourcemaps to the minified version
+ * Save the minified version into /dist/styles/folder
+ * Continue with browserSync logic only after all previous things have finished
+ *
+ ***************************************/
 gulp.task('styles', () => {
-    gulp.src(['src/styles/**/main.less'])
+    gulp.src(['src/styles/main.less'])
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(minifyCss())
@@ -25,17 +42,35 @@ gulp.task('styles', () => {
         .pipe(browserSync.stream());
 });
 
-// uglifying javascript and adding sourcemaps
+/********** JAVASCRIPT ******************
+ *
+ * Read original scripts from /src/scripts/ folder
+ * Create sourcemaps with the original content
+ * Uglify/minify the scripts
+ * Write the sourcemaps to the minified version
+ * Save the minified version into /dist/scripts/folder
+ * Continue with browserSync logic only after all previous things have finished
+ *
+ ***************************************/
 gulp.task('scripts', () => {
     gulp.src(['src/scripts/main.js'])
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/scripts'))
-        .pipe(browserSync.stream())
+        .pipe(browserSync.stream());
 });
 
-// observing changes in the files and reloading browser
+/***** SYNC FILES WITH BROWSER *********
+ *
+ * Run all tasks for the first load (we need certain assets to be precompiled)
+ * Init browserSync server
+ * Run styles task on changes to less files on /src/styles/ folder and subfolders
+ * Run images task on changes to images on /src/img/ folder and subfolders
+ * Run scripts task on changes to JS scripts on /src/scripts/ folder and subfolders
+ * Reload browser on changes to html files on root folder
+ *
+ ***************************************/
 gulp.task('default', () => {
     browserSync.init({
         server: './'
